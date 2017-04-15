3e807a6528fa42a65221de6e7ebb0ebf44dc04e3
NathanYangcn <nathanyang1991@163.com>
2017-04-15 14:15:46 +0800

    change files

--- waterfall/面向过程/waterfall.js
+++ waterfall/面向过程/waterfall.js
@@ -1,40 +1,73 @@
-var $window = $(window);
-var $lis = $('.ct>li');
-
 function render() {
     //瀑布流图片布局渲染方法 ↓
-    var $imgCt = $('.ct'),
-        $items = $('.ct>li'),
-        ctWidth = $imgCt.width(),
-        itemsWidth = $items.outerWidth(true),
-        //初始化 ↓
-        cols = parseInt(ctWidth / itemsWidth),
-        itemsArr = [];
+    var $container = $('.container');
+    var $items = $('.item');
+    var columnN = parseInt( $container.width() / $items.outerWidth(true) );
+    var columnH = [];
 
-    for(var i = 0; i < cols; i++){
-        itemsArr[i] = 0;
+    for(var i = 0; i < columnN; i++){
+        columnH[i] = 0;
     }
     //遍历每个元素，按照规定进行摆放 ↓
     $items.each(function () {
-        var $this = $(this),
-            thisHeight = $this.outerHeight(true),
+        var minHeight = Math.min.apply(null, columnH);
+        var minIndex = columnH.indexOf(minHeight);
 
-            minHeight = Math.min.apply(null, itemsArr),
-            minIndex = itemsArr.indexOf(minHeight);
+        var $this = $(this);
+        var itemsH = $this.outerHeight(true);
+        var itemsW = $this.outerWidth(true);
 
         $this.css({
-            top: minHeight,
-            left: minIndex * itemsWidth
+            top: columnH[minIndex],
+            left: itemsW * minIndex
         });
-        itemsArr[minIndex] += thisHeight;
+
+        columnH[minIndex] += itemsH;
     });
 }
-
 //进入页面初次调用 ↓
-$lis.find('img').on('load', function(){
-    render();
-});
+render();
 //监听窗口宽高的改变，以便重新调用瀑布流函数渲染界面 ↓
-$window.on('resize', function(){
-    render();
-});   
+$(window).on('resize', function () {
+    render()
+})
+
+
+// var $window = $(window);
+// var $lis = $('.ct>li');
+
+// function render() {
+//     //瀑布流图片布局渲染方法 ↓
+//     var $imgCt = $('.ct'),
+//         $items = $('.ct>li'),
+//         ctWidth = $imgCt.width(),
+//         itemsWidth = $items.outerWidth(true),
+//         //初始化 ↓
+//         cols = parseInt(ctWidth / itemsWidth),
+//         itemsArr = [];
+
+//     for(var i = 0; i < cols; i++){
+//         itemsArr[i] = 0;
+//     }
+//     //遍历每个元素，按照规定进行摆放 ↓
+//     $items.each(function () {
+//         var $this = $(this),
+//             thisHeight = $this.outerHeight(true),
+
+//             minHeight = Math.min.apply(null, itemsArr),
+//             minIndex = itemsArr.indexOf(minHeight);
+
+//         $this.css({
+//             top: minHeight,
+//             left: minIndex * itemsWidth
+//         });
+//         itemsArr[minIndex] += thisHeight;
+//     });
+// }
+
+// //进入页面初次调用 ↓
+// render();
+// //监听窗口宽高的改变，以便重新调用瀑布流函数渲染界面 ↓
+// $window.on('resize', function(){
+//     render();
+// });   
